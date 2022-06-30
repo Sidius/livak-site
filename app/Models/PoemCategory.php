4bd9c5b812c80a397dd7a77bdba382544c74e640
Romanets Pavel <pavelroma24@gmail.com>
2022-06-30 19:45:58 +0300

    add: database migrations, model

---
+++ app/Models/PoemCategory.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Cviebrock\EloquentSluggable\Sluggable;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class PoemCategory extends Model
+{
+    use HasFactory, Sluggable;
+
+    protected $fillable = ['title'];
+
+    public function poems()
+    {
+        return $this->hasMany(Poem::class);
+    }
+
+    /**
+     * Return the sluggable configuration array for this model.
+     *
+     * @return array
+     */
+    public function sluggable(): array
+    {
+        return [
+            'slug' => [
+                'source' => 'title'
+            ]
+        ];
+    }
+}
---
+++ database/migrations/2022_06_30_162306_create_poems_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('poems', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title');
+            $table->string('slug')->unique();
+            $table->text('text');
+            $table->integer('poem_category_id')->unsigned();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('poems');
+    }
+};
