cbafd7f20010591603004b1871c8836344fda815
Romanets Pavel <pavelroma24@gmail.com>
2022-07-02 23:37:35 +0300

    up: migration

--- database/migrations/2022_06_30_162306_create_poems_table.php
+++ database/migrations/2022_06_30_162306_create_poems_table.php
@@ -18,7 +18,7 @@ return new class extends Migration
             $table->string('title');
             $table->string('slug')->unique();
             $table->text('text');
-            $table->integer('poem_category_id')->unsigned();
+            $table->integer('poem_category_id')->unsigned()->nullable();
             $table->timestamps();
         });
     }
